5cc81ba32ef7cfab578b4e7ba910fd19e9436b01
陈敏杰 <justforfun24@163.com>
2025-08-10 11:25:37 +0800

    Remove main.py from git tracking

--- main.py
+++
@@ -1,5 +0,0 @@
-import pysnowball as ball
-
-if __name__ == '__main__':
-    ball.set_token("xq_a_token=cd82cf959785ada23a18243e5863b61ff4e950fb")
-    print(ball.rebalancing_history("ZH3334492", 2, 1))
